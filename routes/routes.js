c2ae18a04c1e97c61f4f698ab83e4577fbe341f3
Paulina San Martin <puli.sanmartin@gmail.com>
2024-04-22 21:37:07 -0400

    Ruta post/roommate funcionando correctamente

--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,20 @@ import express from "express";
 const router = express.Router();
 import path from "path";
 const __dirname = import.meta.dirname;
-import { apiRandom } from "../controllers/controllers.js";
+import { addRoommate, verRoommies } from "../controllers/roommates.js";
 
 router.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname, "../views/index.html"));
 });
 
-router.get("/random", apiRandom)
+//rutas botones y listas
+router.post("/roommate", addRoommate );
+router.get("/roommates", verRoommies);
+
+//gastos
+// router.get("/gastos",);
+// router.post("/gasto",);
+// router.put("/gasto",);
+// router.delete("/gasto",)
 
 export default router;
